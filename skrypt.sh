86637397ae7f8a0d7065b86b972fc302ba79cb85
Acafax <rudywiktor.markiewicz1@dsw.edu.pl>
2023-04-23 18:26:45 +0200

    dodano funkcję tworzaca 100 plikow

--- skrypt.sh
+++ skrypt.sh
@@ -18,6 +18,18 @@ if [ "$key" == "--date" ]; then
         exit 1
         ;;
     esac
+elif [ "$key" == "--logs" ];then
+    
+for i in {1..100}
+do
+    nazwa_pliku="file$i.txt"
+    touch "$nazwa_pliku"
+    echo "ten plik został stowrzony przez skrypt o nazwie 'skrypt.sh'" >> "$nazwa_pliku"          
+    echo "ten plik nazywa sie $nazwa_pliku" >> "$nazwa_pliku"
+    echo "jest on plikiem numer $i" >> "$nazwa_pliku"
+    echo "dzisiajsza data to $(date +%Y-%m-%d)" >>"$nazwa_pliku"
+done   
+
 else
     echo "Nie rozpoznano flagi $key"
 fi
