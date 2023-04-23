0326bf1132e1cdb4ab4869d49092f7eececff694
Acafax <rudywiktor.markiewicz1@dsw.edu.pl>
2023-04-23 20:39:36 +0200

    dodano funkcje help

--- skrypt.sh
+++ skrypt.sh
@@ -13,8 +13,6 @@ if [[ $1 == "--logs" && "$2" =~ ^[0-9]+$ && "$2" -ge 0 && "$2" -le 50 && ${2:+tr
     exit 0
 fi
 
-
-
 touch .gitignore
     echo "*log*" >> ".gitignore"
 #-------------------------------
@@ -45,15 +43,12 @@ do
     echo "dzisiajsza data to $(date +%Y-%m-%d)" >>"$nazwa_pliku"
 done   
 #---------------------------------
-elif [[ $1 == "--logs" && "$2" =~ ^[0-9]+$ && "$2" -ge 0 && "$2" -le 50 ]]; then
-    for ((i=1; i<=$2; i++)); do
-        nazwa_pliku="plik$i.txt"
-        touch "$nazwa_pliku"
-        echo "ten plik został stowrzony przez skrypt o nazwie 'skrypt.sh'" >> "$nazwa_pliku"          
-        echo "ten plik nazywa sie $nazwa_pliku" >> "$nazwa_pliku"
-        echo "jest on plikiem numer $i" >> "$nazwa_pliku"
-        echo "dzisiajsza data to $(date +%Y-%m-%d)" >>"$nazwa_pliku"
-    done
+elif [ "$key" = "--help" ]; then
+    echo "komenda '--date' wyświetla dzisiejsza date "
+    echo "komenda '--logs' tworzy 100 plików tekstowych i w kazdym podana jest nazwa tego skryptu, data, numer pliku oraz jego nazwa "
+    echo "komenda '--logs __' robi właściwie to samo co '--logs', ale można wybrać parametr określający ilosc stworzonych plików  "
+    exit 0 
+
 else
     echo "Nie rozpoznano flagi $key"
 fi
